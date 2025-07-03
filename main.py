6d6b4cc3e1cf32394da131dca225f44ac9b66695
Paweł Kępczyński <pawel.k1997@gmail.com>
2025-07-03 15:08:14 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+zakupy = {
+    'piekarnia': ['chleb', 'pączek', 'bułki'],
+    'warzywniak': ['marchew', 'seler', 'rukola']
+}
+
+print(zakupy)
+
+number = 0
+
+for shop, items_list in zakupy.items():
+    capitalized_items = [item.capitalize() for item in items_list]
+    items_string = ", ".join(capitalized_items)
+    number += len(items_list)
+    print(f"Idę do {shop.capitalize()} i kupuję tam {capitalized_items}")
+
+print(f"W sumie kupiję {number} produktów.")
